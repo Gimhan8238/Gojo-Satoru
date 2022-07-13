0f42ef398fcdf405e4d7bab01890ce05e45529fd
Gimhan8238 <109212346+Gimhan8238@users.noreply.github.com>
2022-07-13 15:37:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['+94741064785']
+global.premium = ['+94741064785']
+global.ownernomer = '+94741064785'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
